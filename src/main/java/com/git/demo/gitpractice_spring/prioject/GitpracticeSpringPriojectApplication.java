4ff29b392bedc8260ef21768dfe770333f779422
satish-lead <satishnani75@gmail.com>
2024-07-25 11:21:52 +0530

    first commit

---
+++ src/main/java/com/git/demo/gitpractice_spring/prioject/GitpracticeSpringPriojectApplication.java
@@ -0,0 +1,13 @@
+package com.git.demo.gitpractice_spring.prioject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracticeSpringPriojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracticeSpringPriojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/demo/gitpractice_spring/prioject/GitpracticeSpringPriojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.demo.gitpractice_spring.prioject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracticeSpringPriojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
