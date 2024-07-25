75cfb442b12ec364fe49fd5f5b3f3e9e3d46fbba
satish-lead <satishnani75@gmail.com>
2024-07-25 12:44:32 +0530

    hellowordl

---
+++ src/main/java/com/git/demo/gitpractice_spring/prioject/javaclases/Helloworld.java
@@ -0,0 +1,10 @@
+package com.git.demo.gitpractice_spring.prioject.javaclases;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world 1");
+	}
+
+}
