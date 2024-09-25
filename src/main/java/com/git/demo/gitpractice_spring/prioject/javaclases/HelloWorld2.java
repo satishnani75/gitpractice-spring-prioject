8559992fdbd7deef65987497dc6f637add74677a
satish-dev1 <garapatisatish936@gmail.com>
2024-09-25 07:52:50 +0530

    add 3rd sys from dev brahch

--- src/main/java/com/git/demo/gitpractice_spring/prioject/javaclases/HelloWorld2.java
+++ src/main/java/com/git/demo/gitpractice_spring/prioject/javaclases/HelloWorld2.java
@@ -5,6 +5,8 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println(" hello from helloworld 2 prog by feture branch edited");
 		System.out.println(" hellow added from dev branch");
+		
+		System.out.println(" added from developer brahch 3 ");
 	}
 
 }
