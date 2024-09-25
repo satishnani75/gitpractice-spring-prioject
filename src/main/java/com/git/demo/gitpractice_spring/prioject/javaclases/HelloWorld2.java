eec4ee9287862b381c6d2cbe1294d7994c9b3ecd
satish-dev1 <garapatisatish936@gmail.com>
2024-09-25 07:38:24 +0530

    edit added to syso by dev banch

--- src/main/java/com/git/demo/gitpractice_spring/prioject/javaclases/HelloWorld2.java
+++ src/main/java/com/git/demo/gitpractice_spring/prioject/javaclases/HelloWorld2.java
@@ -3,7 +3,7 @@ package com.git.demo.gitpractice_spring.prioject.javaclases;
 public class HelloWorld2 {
 	
 	public static void main(String[] args) {
-		System.out.println(" hello from helloworld 2 prog by feture branch");
+		System.out.println(" hello from helloworld 2 prog by feture branch edited");
 	}
 
 }
